638ee7bc62943a9eaad19b1806bcc3473b227dbc
Clodonil Honorio Trigo <clodonil@nisled.org>
2017-05-26 14:19:42 -0300

    criando os diretorios automaticamente

--- main.py
+++ main.py
@@ -1,34 +1,53 @@
 import sys
 from libs.bkmagento import Backup_Magento
+import argparse
 
 '''
    Programa principal
 '''
 
 
-params=[]
+# Carga de parametros
+parser = argparse.ArgumentParser(description = 'Script de Backup do Magento no GDrive.')
+parser.add_argument('--magento', action='store_true', dest = 'magento',
+                                                   default = False, required = False,
+                                                   help = 'Coloque essa opcao para fazer backup do magento.')
 
-for opcao in sys.argv:
-    params.append(opcao)
 
-local    = Backup_Magento()
+parser.add_argument('--mysql', action='store_true', dest = 'mysql',
+                                                   default = False, required = False,
+                                                   help = 'Coloque essa opcao para fazer backup do Mysql.')
 
-# Verifica se chegou no numero limite de arquivos para backup
-if not local.check_limit_file():
-    print('Espaco para backup indefinido...')
-    sys.exit(1)
+args = parser.parse_args()
 
-if len(params) == 1  or "mysql" in params:
+def main(args):
+    '''
+       Modulo principal
+    '''
+    local    = Backup_Magento()
+
+    # Verifica se chegou no numero limite de arquivos para backup
+    if not local.check_limit_file():
+       print('Espaco para backup indefinido...')
+       return false
+
     # Backup dos arquivos
-    mysql_file   = local.backup_mysql()
-    # upload dos novos backups
-    if mysql_file != False:
-       local.upload(mysql_file)
+    if args.mysql:
+       mysql_file   = local.backup_mysql()
+       # upload dos novos backups
+       if not mysql_file:
+          local.upload(mysql_file)
 
-if len(params) == 1  or "magento" in params:
-   magento_file = local.backup_magento()
-   if magento_file != False:
-      local.upload(magento_file)
+    if args.magento:
+        magento_file = local.backup_magento()
+        #upload dos novos backups
+        if not magento_file:
+           local.upload(magento_file)
 
-# limpa os arquivos de cache e tmps
-local.coletor_lixo()
+    # limpa os arquivos de cache e tmps
+    local.coletor_lixo()
+    return True
+
+if __name__ == "__main__":
+    if not main(args):
+        print("problema na execucao do script, verifique os logs")
